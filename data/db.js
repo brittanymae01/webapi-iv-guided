37f5bdded58aa319992bf2adf85d9934d1dca933
brittanymae01 <brittanymae01>
2020-01-16 10:40:58 -0800

    another test

--- data/db.js
+++ data/db.js
@@ -1,5 +1,5 @@
 const knex = require("knex");
-const environment = process.env.DB_ENV || "development";
-const config = require("../knexfile.js")[environment];
+
+const config = require("../knexfile.js").development;
 
 module.exports = knex(config);
